52ef018b5e916529a53289837cc7e595722af941
Chris Carey <careych5@Chriss-MacBook-Pro.local>
2017-04-27 19:47:47 -0400

    module introduction

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const {helloLog} = require('./src/log');
 const {helloLog2} = require('./src/log');
 
 // console.log(helloLog());
-export{helloLog, helloLog2};
+module.exports.blah = helloLog;
+module.exports.blah2 = helloLog2;
